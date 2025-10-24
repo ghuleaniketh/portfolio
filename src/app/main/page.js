34e715c55c0970b276fd14f9d13d248b25dda9f9
aniketh ghule <ghuleaniketh@gmail.com>
2025-10-24 23:35:16 +0530

    adding stuff

---
+++ src/app/main/page.js
@@ -0,0 +1,5 @@
+"use client"
+
+export default function Home(){
+    return <h1>This is Aniketh's page</h1>
+}
---
+++ src/componets/index/page.js
@@ -0,0 +1,8 @@
+export default function Index(){
+    return(
+        <div>
+            <p>GHULE ANIKETH</p>
+            <img src="./random/image.jpg" alt="Random Image"></img>
+        </div>
+    )
+}
